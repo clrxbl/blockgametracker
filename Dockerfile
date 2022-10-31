a82df0e4ba1a61ea8a325fbdafb95e99c32b13e4
Michael <michael@iptables.sh>
2022-10-31 13:31:02 +0100

    blockgametracker

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+FROM python:3.9-alpine3.15 AS build-env
+
+ENV PYTHONFAULTHANDLER=1 \
+    PYTHONUNBUFFERED=1 \
+    PYTHONHASHSEED=random \
+    PIP_NO_CACHE_DIR=off \
+    PIP_DISABLE_PIP_VERSION_CHECK=on \
+    PIP_DEFAULT_TIMEOUT=100 \
+    POETRY_NO_INTERACTION=1
+
+RUN apk add --no-cache --virtual .python_deps build-base python3-dev libffi-dev bash && \
+    pip3 install poetry && \
+    apk del .python_deps && \
+    mkdir -p /app/src /app /shared && \
+    poetry config virtualenvs.create false
+
+ADD pyproject.toml /app/pyproject.toml
+
+WORKDIR /app
+
+RUN poetry export --dev --without-hashes --no-interaction --no-ansi -f requirements.txt -o requirements.txt && \
+    pip install --prefix=/runtime --force-reinstall -r requirements.txt
+
+ADD src /app/src
+
+FROM gcr.io/distroless/python3-debian11
+
+ENV PYTHONPATH=/usr/local/lib/python3.9/site-packages
+
+COPY --from=build-env /runtime /usr/local
+
+ADD src /app/src
+WORKDIR /app
+
+ENTRYPOINT python3 /app/src/blockgametracker/main.py
---
+++ src/blockgametracker/main.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+import os
+import sys
+import hiyapyco
+import pathlib
+import time
+import asyncio
+from loguru import logger
+from func_timeout import func_set_timeout, FunctionTimedOut
+from dataclasses import dataclass
+from loguru_logging_intercept import setup_loguru_logging_intercept
+from prometheus_client import start_http_server
+from prometheus_client.core import GaugeMetricFamily, REGISTRY
+from mcstatus import JavaServer, BedrockServer
+from timeit import default_timer as timer
+
+CONFIG_FILE = os.getenv("CONFIG_FILE", "config/servers.yaml")
+
+@dataclass
+class MinecraftServer:
+  """
+  Class for querying a Minecraft server
+  """
+
+  edition: str = "java"
+  name: str = "Minecraft server"
+  address: str = "127.0.0.1"
+  port: int = None # We don't want to set a port otherwise mcstatus does not do SRV lookups
+  playercount: int = None
+  version: int = None
+
+  @func_set_timeout(1)
+  async def query(self):
+    """
+    Get the number of players on the server and protocol version.
+    """
+
+    start = timer()
+
+    if self.port is not None:
+      address = f"{self.address}:{self.port}"
+    else:
+      address = self.address
+
+    try:
+      if self.edition == "java":
+        server = JavaServer.lookup(address)
+      elif self.edition == "bedrock":
+        server = BedrockServer.lookup(address)
+      else:
+        raise NotImplementedError("Unknown server edition")
+
+      status = await server.async_status()
+    except Exception as e:
+      logger.error(f"{self} failed to query: {e}")
+      await logger.complete()
+      return
+
+    self.version = int(status.version.protocol)
+    self.playercount = int(status.players.online)
+
+    end = timer()
+    logger.info(f"Queried {self.name} ({self.address}) in {round((end - start), 2)} seconds")
+    return self.playercount, self.version
+
+
+async def collect_metrics(sem, server):
+  """
+  Actually collect & return the data from the server
+  """
+  
+  async with sem:
+    server = MinecraftServer(name=server["name"], address=server["address"])
+
+    try:
+      await server.query()
+    except FunctionTimedOut:
+      logger.warning(f"{server} timed out")
+      pass
+    
+    return server
+
+
+async def metric_collection(servers):
+  """
+  Collect metrics from all servers
+  """
+
+  sem = asyncio.Semaphore(8)
+
+  workers = [asyncio.create_task(collect_metrics(sem, server)) for server in servers]
+  result = await asyncio.gather(*workers)
+
+  return result
+
+
+class MinecraftCollector(object):
+  """
+  Collects Minecraft server metrics from a list of servers.
+  """
+
+
+  def collect(self):
+    config = hiyapyco.load(CONFIG_FILE)
+    gauge = GaugeMetricFamily("minecraft_status_players_online_count", "Minecraft server online player counts",
+            labels=["server_edition", "server_name", "server_host", "server_version"])
+
+    logger.info("Collecting metrics from " + str(len(config["servers"])) + " servers...")
+    start = timer()
+
+    metrics = asyncio.run(metric_collection(config["servers"]))
+
+    for server in metrics:
+      if server.version is not None and server.playercount is not None:
+        gauge.add_metric([server.edition, server.name, server.address, str(server.version)], server.playercount)
+      else:
+        logger.warning(f"{server} did not return any metrics, not adding to gauge")
+    end = timer()
+    logger.info(f"Finished collecting metrics in {round((end - start), 2)} seconds")
+    yield gauge
+
+
+@logger.catch
+def main(log_level):
+    # Logging
+    logger.remove()
+    logger.add(
+        sys.stderr,
+        colorize=True,
+        level=log_level.upper(),
+        backtrace=True,
+        diagnose=True,
+    )
+
+    setup_loguru_logging_intercept(
+        level=log_level.upper(),
+    )
+
+    start_http_server(8080)
+    REGISTRY.register(MinecraftCollector())
+
+    while True:
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+  log_level = os.getenv("LOG_LEVEL", "INFO")
+  main(log_level)
